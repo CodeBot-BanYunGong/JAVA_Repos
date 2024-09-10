3c4b0f1727e24e14c3a211816de7dde1c3673f47
CodeBot-BanYunGong <pete8467005@163.com>
2024-09-10 13:06:42 +0800

    Update and rename CellsetCreate.java to Cellset_Create.java

--- CellSetCreation/Cellset_Create.java
+++ CellSetCreation/Cellset_Create.java
@@ -1,4 +1,4 @@
-/*
+/**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -18,10 +18,11 @@ import star.base.report.*;
 import star.meshing.*;
 
 /**
- * This CCM macro creates probe points in a CCM model. The probe points are defined
- * in a csv file and read into the macro. Sim, csv and java macro should all be located
- * in the same directory. The csv file format contains a header in row 1.
- * Probe point data should start in row 2. csv file format information:
+ * This CCM macro creates CellSet and ThresholdPart in a CCM model. 
+ * The CellSet coordinates are defined in a csv file and read into the macro. 
+ * csv and java macro should be located in the same directory. 
+ * The csv file format contains a header in row 1.
+ * Coordinates data should start in row 2. csv file format information:
  * Column 1: x location of probe point [mm]
  * Column 2: y location of probe point [mm]
  * Column 3: z location of probe point [mm]
@@ -29,15 +30,11 @@ import star.meshing.*;
  * Column 5: y location of probe point [mm]
  * Column 6: z location of probe point [mm]
  * Column 7: cellset name
- * 
- *
- * @author mobrigke
- * 
- * Updated 11/22/2021: Added ability to handle mesh parts that have a '.' in the name. Prior version assumed all part names that included
- * a '.' were composite parts.
- * 
+ * Column 8: part name in ThresholdPart properties, that may be part or region
+ * Column 9: part type, only fill "Part" or "Region"
+ * Column 10: threshold part name
  */
-public class CellsetCreate extends StarMacro {
+public class Cellset_Create extends StarMacro {
 
     /**
      * @param args the command line arguments
@@ -87,16 +84,6 @@ public class CellsetCreate extends StarMacro {
 				
                 n++;
 				
-				/** Log the values to debug
-                sim.println("Processing row: " + n);
-                sim.println("xp1: " + xp1 + ", yp1: " + yp1 + ", zp1: " + zp1);
-                sim.println("xp2: " + xp2 + ", yp2: " + yp2 + ", zp2: " + zp2);
-                sim.println("CellSetName: " + CellSetName);
-                sim.println("PartName: " + PartName);
-                sim.println("PartType: " + PartType);
-                sim.println("ThresholdName: " + ThresholdName);
-				*/
-                
                 // Create cellset with given coordinates
                 LabCoordinateSystem labCoordinateSystem_0 = sim.getCoordinateSystemManager().getLabCoordinateSystem();
                 FvRepresentation fvRepresentation_0 = ((FvRepresentation) sim.getRepresentationManager().getObject("Volume Mesh"));
@@ -109,100 +96,84 @@ public class CellsetCreate extends StarMacro {
 				
                 // Create threshold for cellset based on Geometry Parts
                 if (PartType.equals("Part")) {
-                    MeshPart meshPart_0 = ((MeshPart) sim.get(SimulationPartManager.class).getPart(PartName));
-					
-					sim.println("**MeshPart** found: " + meshPart_0.getPresentationName());
+					MeshPart meshPart_0 = null;
+                    try {
+						meshPart_0 = ((MeshPart) sim.get(SimulationPartManager.class).getPart(PartName));
+						sim.println("**MeshPart** found: " + meshPart_0.getPresentationName());
+					} catch (Exception ex) {
+						sim.println("There no part " + PartName + " found.");
+					}
 					
 					Units units_1 = sim.getUnitsManager().getPreferredUnits(Dimensions.Builder().build());
 					
 					try {
-						// Get PrimitiveFieldFunction from FieldFunctionManager
-						Collection<FieldFunction> fieldFunctions = sim.getFieldFunctionManager().getObjects();
-						Pattern pattern = Pattern.compile(".*" + CellSetName + ".*");// Adjust pattern as needed
-						
-						//sim.println("Available field functions:");
-						
-						FieldFunction matchedFieldFunction = null;
-						for (FieldFunction ff : fieldFunctions){
-							if (pattern.matcher(ff.getPresentationName()).matches()){
-								
-								//sim.println(" - " + ff.getPresentationName() + " (" + ff.getType() + ") ");
-								
-								matchedFieldFunction = ff;
-								break;
-							}
-						}
+						// get fieldFunction related to cellSet_0
+						FieldFunctionManager fieldFunctionManager = sim.getFieldFunctionManager();
+						FieldFunction matchedFieldFunction = fieldFunctionManager.getFunction(cellSet_0.getFunctionName());
 						
 						// Check if get fieldFunction
 						if (matchedFieldFunction != null) {
 							sim.println("FieldFunction found: " + matchedFieldFunction.getPresentationName() + " (" + matchedFieldFunction.getType() + ") ");
 							
 							// Create ThresholdPart with primitiveFieldFunction
+							if (meshPart_0 != null) {
 							ThresholdPart thresholdPart_0 = sim.getPartManager().createThresholdPart(new NeoObjectVector(new Object[] {meshPart_0}), new DoubleVector(new double[] {1.0, 1.0}), units_1, matchedFieldFunction, 0, null);
 							thresholdPart_0.setPresentationName(ThresholdName);
-							
 							sim.println("ThresholdPart : " + thresholdPart_0 + " is created @ MeshPart");
+							} else {
+								sim.println("**ATTENTION**: MeshPart is null, cannot create ThresholdPart.");
+							}
 						} else {
-							sim.println("FieldFunction is not found.");
+							sim.println("**ATTENTION**: FieldFunction is not found.");
 						}
 					} catch (Exception e) {
 						sim.println("Error retrieving PrimitiveFieldFunction: " + e.getMessage());
 						e.printStackTrace();
 					}
 					sim.println("------------------------------\n");
-				
 				} else if (PartType.equals("Region")) {
-                    Region region_0 = sim.getRegionManager().getRegion(PartName);
-					
-					sim.println("**Region** found: " + region_0.getPresentationName());
-        
-                    Units units_1 = sim.getUnitsManager().getPreferredUnits(Dimensions.Builder().build());
-        
+					Region region_0 = null;
 					try {
-						// Get PrimitiveFieldFunction from FieldFunctionManager
-						Collection<FieldFunction> fieldFunctions = sim.getFieldFunctionManager().getObjects();
-						Pattern pattern = Pattern.compile(".*" + CellSetName + ".*");// Adjust pattern as needed
-						
-						//sim.println("Available field functions:");
-						
-						FieldFunction matchedFieldFunction = null;
-						for (FieldFunction ff : fieldFunctions){
-							if (pattern.matcher(ff.getPresentationName()).matches()){
-								
-								//sim.println(" - " + ff.getPresentationName() + " (" + ff.getType() + ") ");
-								
-								matchedFieldFunction = ff;
-								break;
-							}
-						}
+						region_0 = sim.getRegionManager().getRegion(PartName);
+						sim.println("**Region** found: " + region_0.getPresentationName());
+					} catch (Exception ex) {
+						sim.println("There no part " + PartName + " found.");
+					}
+					Units units_1 = sim.getUnitsManager().getPreferredUnits(Dimensions.Builder().build());
+					try {
+						// get fieldFunction related to cellSet_0
+						FieldFunctionManager fieldFunctionManager = sim.getFieldFunctionManager();
+						FieldFunction matchedFieldFunction = fieldFunctionManager.getFunction(cellSet_0.getFunctionName());
 						
 						// Check if get fieldFunction
 						if (matchedFieldFunction != null) {
 							sim.println("FieldFunction found: " + matchedFieldFunction.getPresentationName() + " (" + matchedFieldFunction.getType() + ") ");
 							
 							// Create ThresholdPart with primitiveFieldFunction
+							if (region_0 != null) {
 							ThresholdPart thresholdPart_0 = sim.getPartManager().createThresholdPart(new NeoObjectVector(new Object[] {region_0}), new DoubleVector(new double[] {1.0, 1.0}), units_1, matchedFieldFunction, 0, null);
 							thresholdPart_0.setPresentationName(ThresholdName);
-							
 							sim.println("ThresholdPart : " + thresholdPart_0 + " is created @ Region");
-						} else {
-							sim.println("FieldFunction is not found.");
-						}
-					} catch (Exception e) {
-						sim.println("Error retrieving PrimitiveFieldFunction: " + e.getMessage());
-						e.printStackTrace();
-					}
+								} else {
+									sim.println("**ATTENTION**: Region is null, cannot create ThresholdPart.");
+									}
+							} else {
+								sim.println("**ATTENTION**: FieldFunction is not found.");
+								}
+							} catch (Exception e) {
+								sim.println("Error retrieving PrimitiveFieldFunction: " + e.getMessage());
+								e.printStackTrace();
+								}
 					sim.println("------------------------------\n");
-					
-                } else {
-                    sim.println("No condition met for PartType: " + PartType);
-                }
-            }
+				} else {
+					sim.println("No condition met for PartType: " + PartType);
+				}
+			}
 			try {
-                    Thread.sleep(2000); // 延迟 2 秒
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+				Thread.sleep(2000); // 延迟 2 秒
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					}
 			sim.println("Total quatity < " + n + " > CellSet and ThresholdPart are Created.");
 			sim.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
         } catch (Exception ex) {
